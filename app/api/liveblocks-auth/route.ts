4d7ca9bcb985286ef29ebdfd8a4f89fa00be9be7
thanavreddy <thanavreddy196@gmail.com>
2025-03-31 12:40:52 +0530

    fixed liveblocks auth errors

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -4,14 +4,12 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 export async function POST(request: Request) {
     const clerkUser  = await currentUser();
-  // Get the current user from your database
+// Get the current user from your database
 //   const user = __getUserFromDB__(request);
 if(!clerkUser){
     redirect('/sign-in')
 }
-
 const {id  , firstName ,lastName,emailAddresses,imageUrl} = clerkUser;
-
   const user = {id,
     info : {
         id,
@@ -19,22 +17,18 @@ const {id  , firstName ,lastName,emailAddresses,imageUrl} = clerkUser;
         email :  emailAddresses[0].emailAddress,
         avatar : imageUrl,
         color: getUserColor(id),
-
     }
   }
-
-  // Start an auth session inside your endpoint
+// Start an auth session inside your endpoint
 //   const session = liveblocks.prepareSession(
 //     user.id,
 //     { userInfo: user.metadata } // Optional
 //   );
-
-  // Use a naming pattern to allow access to rooms with wildcards
-  // Giving the user read access on their org, and write access on their group
+// Use a naming pattern to allow access to rooms with wildcards
+// Giving the user read access on their org, and write access on their group
 //   session.allow(`${user.organization}:*`, session.READ_ACCESS);
 //   session.allow(`${user.organization}:${user.group}:*`, session.FULL_ACCESS);
-
-  // Authorize the user and return the result
+// Authorize the user and return the result
   const { status, body } = await liveblocks.identifyUser(
     {
         userId : user.info.email,
